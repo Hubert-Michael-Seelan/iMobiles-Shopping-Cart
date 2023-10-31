5d083ea078bfe4eec9f447f56617260833894617
Hubert_Michael_Seelan <hubertmichaelseelan@gmail.com>
2023-10-31 12:44:28 +0530

    first commit

---
+++ src/products.js
@@ -0,0 +1,58 @@
+export const Products = [
+  {
+    id: 1,
+    productsImage:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone14pro-202209?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1663611329492",
+    productName: "iPhone 13pro",
+    price: 260,
+  },
+  {
+    id: 2,
+    productsImage:
+      "https://cms.vodafone.com.tr/static/img/content/22-04/12/apple-iphone-12-pro-grafit-0.png.webp?w=110",
+    productName: "iPhone 12pro",
+    price: 220,
+  },
+  {
+    id: 3,
+    productsImage:
+      "https://cms.vodafone.com.tr/static/img/content/22-04/12/apple-iphone-12-pro-grafit-0.png.webp?w=110",
+    productName: "iPhone 11pro",
+    price: 200,
+  },
+  {
+    id: 4,
+    productsImage:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone13-202209?wid=450&hei=300&fmt=p-jpg&qlt=95&.v=1661958176452",
+    productName: "iPhone 13",
+    price: 240,
+  },
+  {
+    id: 5,
+    productsImage:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone12-digitalmat-gallery-2-202111?wid=450&hei=300&fmt=png-alpha&.v=1635178709000",
+    productName: "iPhone 12",
+    price: 210,
+  },
+  {
+    id: 8,
+    productsImage:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone13-202209?wid=450&hei=300&fmt=p-jpg&qlt=95&.v=1661958176452",
+    productName: "iPhone 11",
+    price: 155,
+  },
+  {
+    id: 6,
+    productsImage:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphonese-202203?wid=450&hei=300&fmt=p-jpg&qlt=95&.v=1646415838921",
+    productName: "iPhone SE",
+    price: 130,
+  },
+  {
+    id: 7,
+    productsImage:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-compare-iphone-14-202209?wid=450&hei=300&fmt=jpeg&qlt=90&.v=1660759995969",
+    productName: "iPhone 13plus",
+    price: 250,
+  },
+];
